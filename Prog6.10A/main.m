a39ae15d5ca491c47bc0443a7b0c7bba72126018
admin <dangtinhtong@gmail.com>
2017-11-29 22:07:50 +0700

    First commit

--- Prog6.10A/main.m
+++ Prog6.10A/main.m
@@ -5,13 +5,33 @@
 //  Created by admin on 11/29/17.
 //  Copyright © 2017 admin. All rights reserved.
 //
+// Program to generatea table of prime numbers
+// Second version using BOOL type and predefined values
 
 #import <Foundation/Foundation.h>
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
-        NSLog(@"Hello, World!");
+        //NSLog(@"Hello, World!");
+        
+        int p,d;
+        BOOL isPrime;
+        for(p=2;p<=50;++p)
+        {
+            isPrime= YES;
+            
+            for(d=2;d<p;++d)
+                
+                if(p%d==0)
+                    
+                    isPrime=NO;
+            
+            if(isPrime==YES)
+                
+                NSLog(@"%i",p);
+        }
     }
+    
     return 0;
 }
